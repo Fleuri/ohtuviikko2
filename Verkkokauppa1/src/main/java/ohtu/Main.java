6dc493dfd45894dca6e43a8f5f5b6da8cc705403
laursuom <laur1.suomalainen@gmail.com>
2013-04-05 19:31:17 +0300

    Paskensin koodia yhä lisää

--- Verkkokauppa1/src/main/java/ohtu/Main.java
+++ Verkkokauppa1/src/main/java/ohtu/Main.java
@@ -40,6 +40,11 @@ public class Main {
             } else if (komento == 3) {
                 kirjanpito.getTapahtumat();
             }
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 2; j++) {
+                    System.out.println("uli");
+                }
+            }
         }
     }
 }
