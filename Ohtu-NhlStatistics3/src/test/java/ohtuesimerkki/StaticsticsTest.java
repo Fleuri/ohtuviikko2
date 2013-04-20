f1ebe54c65d1bfb2404f29c7cf03159e4e02a644
Lauri <Lauri@192.168.2.5>
2013-04-20 16:39:32 +0300

    StatisticsTestit MOCKATTU! OH SNAP TROLOLOLOLO LE EBINZ TROLLERIE!

--- Ohtu-NhlStatistics3/src/test/java/ohtuesimerkki/StaticsticsTest.java
+++ Ohtu-NhlStatistics3/src/test/java/ohtuesimerkki/StaticsticsTest.java
@@ -4,32 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.*;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class StaticsticsTest {
 
     Statistics stats;
-    PlayerReader palayerReader = new PlayerReader() {
+    PlayerReader reader = mock(PlayerReader.class); 
+    ArrayList<Player> lista;
 
-        public List<Player> getPlayers() {
-            ArrayList<Player> players = new ArrayList<Player>();
-
-            players.add(new Player("Semenko", "EDM", 4, 12));
-            players.add(new Player("Lemieux", "PIT", 45, 54));
-            players.add(new Player("Kurri", "EDM", 37, 53));
-            players.add(new Player("Yzerman", "DET", 42, 56));
-            players.add(new Player("Gretzky", "EDM", 35, 89));
-
-            return players;
-        }
-    };
 
     @Before
     public void setUp() {
-        stats = new Statistics(palayerReader);
+            lista = new ArrayList<Player>();
+            lista.add(new Player("Semenko", "EDM", 4, 12));
+            lista.add(new Player("Lemieux", "PIT", 45, 54));
+            lista.add(new Player("Kurri", "EDM", 37, 53));
+            lista.add(new Player("Yzerman", "DET", 42, 56));
+            lista.add(new Player("Gretzky", "EDM", 35, 89));
+            when(reader.getPlayers()).thenReturn(lista);
+            stats = new Statistics(reader);
+        
+        
     }
 
     @Test
     public void playerFound() {
+        
         Player p = stats.search("Lemieux");
         assertEquals("PIT", p.getTeam());
         assertEquals(45, p.getGoals());
@@ -40,6 +40,7 @@ public class StaticsticsTest {
     
     @Test
     public void teamMembersFound(){
+        when(reader.getPlayers()).thenReturn(lista);
         List<Player> players = stats.team("EDM");
         assertEquals(3, players.size());
         for (Player player : players) {
@@ -49,6 +50,7 @@ public class StaticsticsTest {
     
     @Test
     public void topScorersFound(){
+        when(reader.getPlayers()).thenReturn(lista);
         List<Player> players = stats.topScorers(2);
         assertEquals(3, players.size());
         assertEquals("Gretzky", players.get(0).getName());
